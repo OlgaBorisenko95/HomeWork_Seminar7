223d71459c9b8db014d5c05cd06ea9b0a9d6bc26
Olga Borisenko <olka_borisenko@mail.ru>
2022-12-27 19:06:51 +0300

    Нашли случайные числа двумерного массива

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿// Задача 47. Задайте двумерный массив размером m×n, заполненный случайными вещественными числами.
+// m = 3, n = 4.
+// 0,5 7 -2 -0,2
+// 1 -3,3 8 -9,9
+// 8 7,8 -7,1 9
+
+void Array(int m, int n)
+{
+    double[,] array = new double[m, n];
+    for (int i = 0; i < m; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            array[i, j] = new Random().NextDouble()*10; 
+            Console.Write($"{Math.Round(array[i, j], 1)}    ");
+        }
+        Console.WriteLine();
+    }
+}
+Console.Write("Введите число строк массива: ");
+int m = int.Parse(Console.ReadLine());
+Console.Write("Введите число столбцов массива: ");
+int n = int.Parse(Console.ReadLine());
+Array(m, n);
